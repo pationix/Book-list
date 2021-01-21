112b9758d4a91ba6731e2bc31f0bbb7db30d4667
pationix <makopat@gmail.com>
2021-01-22 00:27:37 +0100

    poprawa bazy danych

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
-    readed = models.DateTimeField('date readed')
-    review = models.CharField(max_length=200)
+    author = models.CharField(max_length=50, null=True, blank=True)
+    readed = models.DateField(default=None, null=True, blank=True)
+    review = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/<int:book_id', views.book_by_id, name='book_by_id'),
 ]
