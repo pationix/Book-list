e71672b6ce536ac6d8b5def95e7aa718f5322cae
pationix <makopat@gmail.com>
2021-01-22 23:37:04 +0100

    Edycja książki

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('update_book/<str:pk>/', views.updateBook, name="update_book")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,3 +15,18 @@ def home(request):
 
     context = {"books":books, 'form':form}
     return render(request, "homepage.html", context)
+
+def updateBook(request, pk):
+    book = Book.objects.get(id=pk)
+
+    form = BookForm(instance=book)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+
+    return render(request, 'update_book.html', context)
